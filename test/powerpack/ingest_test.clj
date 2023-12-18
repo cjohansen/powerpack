3f25375b918a20459611b95eab5dfa4154a690e7
Christian Johansen <christian@cjohansen.no>
2023-12-18 12:21:31 +0100

    Use a lintable form for the with-schema-db test helper

--- test/powerpack/ingest_test.clj
+++ test/powerpack/ingest_test.clj
@@ -17,7 +17,7 @@
          (finally
            (d/delete-database uri#))))))
 
-(defmacro with-schema-db [schema binding & body]
+(defmacro with-schema-db [[binding schema] & body]
   `(do
      (let [uri# (str "datomic:mem://" (random-uuid))]
        (d/create-database uri#)
@@ -31,71 +31,63 @@
 
 (deftest align-with-schema
   (testing "Parses numbers from strings"
-    (is (= (with-schema-db [{:db/ident :person/age
-                             :db/valueType :db.type/bigdec
-                             :db/cardinality :db.cardinality/one}]
-             db
+    (is (= (with-schema-db [db [{:db/ident :person/age
+                                 :db/valueType :db.type/bigdec
+                                 :db/cardinality :db.cardinality/one}]]
              (sut/align-with-schema {:person/age "1.0M"} db))
            {:person/age 1.0M}))
 
-    (is (= (with-schema-db [{:db/ident :person/age
-                             :db/valueType :db.type/bigint
-                             :db/cardinality :db.cardinality/one}]
-             db
+    (is (= (with-schema-db [db [{:db/ident :person/age
+                                 :db/valueType :db.type/bigint
+                                 :db/cardinality :db.cardinality/one}]]
              (sut/align-with-schema {:person/age "7N"} db))
            {:person/age 7N}))
 
-    (is (= (-> (with-schema-db [{:db/ident :person/age
-                                 :db/valueType :db.type/double
-                                 :db/cardinality :db.cardinality/one}]
-                 db
+    (is (= (-> (with-schema-db [db [{:db/ident :person/age
+                                     :db/valueType :db.type/double
+                                     :db/cardinality :db.cardinality/one}]]
                  (sut/align-with-schema {:person/age "3.2"} db))
                :person/age
                type)
            java.lang.Double))
 
-    (is (= (->> (with-schema-db [{:db/ident :person/ages
-                                  :db/valueType :db.type/double
-                                  :db/cardinality :db.cardinality/many}]
-                  db
+    (is (= (->> (with-schema-db [db [{:db/ident :person/ages
+                                      :db/valueType :db.type/double
+                                      :db/cardinality :db.cardinality/many}]]
                   (sut/align-with-schema {:person/ages "[3.2 1.2]"} db))
                 :person/ages
                 (map type))
            [java.lang.Double
             java.lang.Double]))
 
-    (is (= (-> (with-schema-db [{:db/ident :person/age
-                                 :db/valueType :db.type/float
-                                 :db/cardinality :db.cardinality/one}]
-                 db
+    (is (= (-> (with-schema-db [db [{:db/ident :person/age
+                                     :db/valueType :db.type/float
+                                     :db/cardinality :db.cardinality/one}]]
                  (sut/align-with-schema {:person/age "3.2"} db))
                :person/age
                type)
            java.lang.Float))
 
-    (is (= (->> (with-schema-db [{:db/ident :person/ages
-                                  :db/valueType :db.type/float
-                                  :db/cardinality :db.cardinality/many}]
-                  db
+    (is (= (->> (with-schema-db [db [{:db/ident :person/ages
+                                      :db/valueType :db.type/float
+                                      :db/cardinality :db.cardinality/many}]]
                   (sut/align-with-schema {:person/ages "[3.2 1.2]"} db))
                 :person/ages
                 (map type))
            [java.lang.Float
             java.lang.Float]))
 
-    (is (= (-> (with-schema-db [{:db/ident :person/age
-                                 :db/valueType :db.type/long
-                                 :db/cardinality :db.cardinality/one}]
-                 db
+    (is (= (-> (with-schema-db [db [{:db/ident :person/age
+                                     :db/valueType :db.type/long
+                                     :db/cardinality :db.cardinality/one}]]
                  (sut/align-with-schema {:person/age "3"} db))
                :person/age
                type)
            java.lang.Long))
 
-    (is (= (->> (with-schema-db [{:db/ident :person/ages
-                                  :db/valueType :db.type/long
-                                  :db/cardinality :db.cardinality/one}]
-                  db
+    (is (= (->> (with-schema-db [db [{:db/ident :person/ages
+                                      :db/valueType :db.type/long
+                                      :db/cardinality :db.cardinality/one}]]
                   (sut/align-with-schema {:person/ages "[3 2]"} db))
                 :person/ages
                 (map type))
@@ -103,118 +95,105 @@
             java.lang.Long])))
 
   (testing "Parses boolean"
-    (is (= (with-schema-db [{:db/ident :person/of-age?
-                             :db/valueType :db.type/boolean
-                             :db/cardinality :db.cardinality/one}]
-             db
+    (is (= (with-schema-db [db [{:db/ident :person/of-age?
+                                 :db/valueType :db.type/boolean
+                                 :db/cardinality :db.cardinality/one}]]
              (sut/align-with-schema {:person/of-age? "true"} db))
            {:person/of-age? true})))
 
   (testing "Parses instant"
-    (is (= (with-schema-db [{:db/ident :person/date-of-birth
-                             :db/valueType :db.type/instant
-                             :db/cardinality :db.cardinality/one}]
-             db
+    (is (= (with-schema-db [db [{:db/ident :person/date-of-birth
+                                 :db/valueType :db.type/instant
+                                 :db/cardinality :db.cardinality/one}]]
              (sut/align-with-schema {:person/date-of-birth "#inst \"2017-09-16T11:43:32.450-00:00\""} db))
            {:person/date-of-birth #inst "2017-09-16T11:43:32.450-00:00"})))
 
   (testing "Parses keyword"
-    (is (= (with-schema-db [{:db/ident :person/ident
-                             :db/valueType :db.type/keyword
-                             :db/cardinality :db.cardinality/one}]
-             db
+    (is (= (with-schema-db [db [{:db/ident :person/ident
+                                 :db/valueType :db.type/keyword
+                                 :db/cardinality :db.cardinality/one}]]
              (sut/align-with-schema {:person/ident ":person/christian"} db))
            {:person/ident :person/christian})))
 
   (testing "Parses many keywords"
-    (is (= (with-schema-db [{:db/ident :person/idents
-                             :db/valueType :db.type/keyword
-                             :db/cardinality :db.cardinality/many}]
-             db
+    (is (= (with-schema-db [db [{:db/ident :person/idents
+                                 :db/valueType :db.type/keyword
+                                 :db/cardinality :db.cardinality/many}]]
              (sut/align-with-schema {:person/idents "[:person/christian :person/chris]"} db))
            {:person/idents [:person/christian :person/chris]})))
 
   (testing "Parses symbol"
-    (is (= (with-schema-db [{:db/ident :person/nickname
-                             :db/valueType :db.type/symbol
-                             :db/cardinality :db.cardinality/one}]
-             db
+    (is (= (with-schema-db [db [{:db/ident :person/nickname
+                                 :db/valueType :db.type/symbol
+                                 :db/cardinality :db.cardinality/one}]]
              (sut/align-with-schema {:person/nickname "chris"} db))
            {:person/nickname 'chris})))
 
   (testing "Parses tuple"
-    (is (= (with-schema-db [{:db/ident :person/nicknames
-                             :db/valueType :db.type/tuple
-                             :db/tupleTypes [:db.type/string :db.type/string]
-                             :db/cardinality :db.cardinality/one}]
-             db
+    (is (= (with-schema-db [db [{:db/ident :person/nicknames
+                                 :db/valueType :db.type/tuple
+                                 :db/tupleTypes [:db.type/string :db.type/string]
+                                 :db/cardinality :db.cardinality/one}]]
              (sut/align-with-schema {:person/nicknames "[\"chris\" \"man\"]"} db))
            {:person/nicknames ["chris" "man"]})))
 
   (testing "Parses uuid"
-    (is (= (with-schema-db [{:db/ident :person/id
-                             :db/valueType :db.type/uuid
-                             :db/cardinality :db.cardinality/one}]
-             db
+    (is (= (with-schema-db [db [{:db/ident :person/id
+                                 :db/valueType :db.type/uuid
+                                 :db/cardinality :db.cardinality/one}]]
              (sut/align-with-schema {:person/id "#uuid \"40af375b-0fed-44fb-a60c-98d9c3d0e851\""} db))
            {:person/id #uuid "40af375b-0fed-44fb-a60c-98d9c3d0e851"})))
 
   (testing "Parses URI"
-    (is (= (with-schema-db [{:db/ident :person/url
-                             :db/valueType :db.type/uri
-                             :db/cardinality :db.cardinality/one}]
-             db
+    (is (= (with-schema-db [db [{:db/ident :person/url
+                                 :db/valueType :db.type/uri
+                                 :db/cardinality :db.cardinality/one}]]
              (sut/align-with-schema {:person/url "https://cjohansen.no"} db))
            {:person/url (java.net.URI/create "https://cjohansen.no")})))
 
   (testing "Parses many URIs"
-    (is (= (with-schema-db [{:db/ident :person/urls
-                             :db/valueType :db.type/uri
-                             :db/cardinality :db.cardinality/many}]
-             db
+    (is (= (with-schema-db [db [{:db/ident :person/urls
+                                 :db/valueType :db.type/uri
+                                 :db/cardinality :db.cardinality/many}]]
              (sut/align-with-schema {:person/urls "[\"https://cjohansen.no\" \"https://mattilsynet.no\"]"} db))
            {:person/urls [(java.net.URI/create "https://cjohansen.no")
                           (java.net.URI/create "https://mattilsynet.no")]})))
 
   (testing "Parses ref"
-    (is (= (with-schema-db [{:db/ident :person/id
-                             :db/valueType :db.type/uuid
-                             :db/cardinality :db.cardinality/one
-                             :db/unique :db.unique/identity}
-                            {:db/ident :person/friend
-                             :db/valueType :db.type/ref
-                             :db/cardinality :db.cardinality/one}]
-             db
+    (is (= (with-schema-db [db [{:db/ident :person/id
+                                 :db/valueType :db.type/uuid
+                                 :db/cardinality :db.cardinality/one
+                                 :db/unique :db.unique/identity}
+                                {:db/ident :person/friend
+                                 :db/valueType :db.type/ref
+                                 :db/cardinality :db.cardinality/one}]]
              (sut/align-with-schema {:person/friend "{:person/id #uuid \"40af375b-0fed-44fb-a60c-98d9c3d0e851\"}"} db))
            {:person/friend {:person/id #uuid "40af375b-0fed-44fb-a60c-98d9c3d0e851"}})))
 
   (testing "Parses multiple refs"
-    (is (= (with-schema-db [{:db/ident :person/id
-                             :db/valueType :db.type/string
-                             :db/cardinality :db.cardinality/one
-                             :db/unique :db.unique/identity}
-                            {:db/ident :person/friends
-                             :db/valueType :db.type/ref
-                             :db/cardinality :db.cardinality/many}]
-             db
+    (is (= (with-schema-db [db [{:db/ident :person/id
+                                 :db/valueType :db.type/string
+                                 :db/cardinality :db.cardinality/one
+                                 :db/unique :db.unique/identity}
+                                {:db/ident :person/friends
+                                 :db/valueType :db.type/ref
+                                 :db/cardinality :db.cardinality/many}]]
              (sut/align-with-schema {:person/friends "[{:person/id \"p1\"} {:person/id \"p2\"}]"} db))
            {:person/friends [{:person/id "p1"}
                              {:person/id "p2"}]})))
 
   (testing "Parses java-time literals"
-    (is (= (with-schema-db [{:db/ident :person/birthday
-                             :dte/valueType :java.time/local-date
-                             :db/cardinality :db.cardinality/one}]
-             db
+    (is (= (with-schema-db [db [{:db/ident :person/birthday
+                                 :dte/valueType :java.time/local-date
+                                 :db/cardinality :db.cardinality/one}]]
              (sut/align-with-schema {:person/birthday "#time/ld \"2007-12-03\""} db))
            {:person/birthday #time/ld "2007-12-03"})))
 
   (testing "Fails on many attribute with a single value"
     (is (= (try
-             (with-schema-db [{:db/ident :person/tags
-                               :db/valueType :db.type/keyword
-                               :db/cardinality :db.cardinality/many}]
-                 db
+             (with-schema-db [db [{:db/ident :person/tags
+                                   :db/valueType :db.type/keyword
+                                   :db/cardinality :db.cardinality/many}]]
                (sut/align-with-schema {:person/tags ":tag1 :tag2"} db))
              (catch Exception e
                {:message (.getMessage e)
